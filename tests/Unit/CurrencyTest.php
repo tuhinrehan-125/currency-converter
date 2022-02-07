37ac1e146fae84486acae52f3ff2de59b0237909
Tuhin Rehan <tuhin.rehan@netmediamobile.com>
2022-02-07 23:14:20 +0600

    currency unit test has been created

---
+++ tests/Unit/CurrencyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+// use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
